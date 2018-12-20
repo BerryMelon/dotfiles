4985c8cd3767fa1625815a30807d07b3115d3472
BerryMelon <berrymelon@me.com>
2018-12-20 03:09:54 +0000

    changed ubuntu_install

--- Bootstrap/ubuntu_install.sh
+++ Bootstrap/ubuntu_install.sh
@@ -6,38 +6,23 @@ echo "Berrymelon's bootstrap setups now starting"
 sudo apt-get update
 sudo apt-get upgrade
 
-PACKAGES=(
-    autoconf
-    git
-    markdown
-    openssl
-    readline
-    sqlite3
-    xz
-    zlib
-    npm
-    python
-    python3
-    ssh-copy-id
-    vim
-    tmux
-    wget
-    zsh
-    zsh-completions
-    figlet
-    node
-    pyenv
-    pyenv-virtualenv
-    pkg-config
-    ruby
-    cmake
-    ctags
-    mosh
-    --with-clang llvm
-)
-
 echo "installing packages..."
 sudo apt-get install ${PACKAGES[@]} -y
+sudo apt-get install git -y
+sudo apt-get install python -y
+sudo apt-get install python3 -y
+sudo apt-get install tmux -y
+sudo apt-get install zsh -y
+sudo apt-get install zsh-completions -y
+sudo apt-get install figlet -y
+sudo apt-get install pyenv -y
+sudo apt-get install pyenv-virtualenv -y
+sudo apt-get install cmake -y
+sudo apt-get install ctags -y
+
+sudo apt-get install python-software-properties
+sudo add-apt-repository ppa:keithw/mosh
+sudo apt-get install mosh -y
 
 echo "Installing and setting up pyenv"
 pyenv install 2.7.10
