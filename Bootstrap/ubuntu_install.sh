0aa8c71a2b8a4c0c87cb9f6429802191c8dd8d9e
BerryMelon <berrymelon@me.com>
2018-12-20 03:34:44 +0000

    minor

--- Bootstrap/ubuntu_install.sh
+++ Bootstrap/ubuntu_install.sh
@@ -15,14 +15,23 @@ sudo apt-get install tmux -y
 sudo apt-get install zsh -y
 sudo apt-get install zsh-completions -y
 sudo apt-get install figlet -y
-sudo apt-get install pyenv -y
-sudo apt-get install pyenv-virtualenv -y
 sudo apt-get install cmake -y
 sudo apt-get install ctags -y
 
 sudo apt-get install python-software-properties
 sudo add-apt-repository ppa:keithw/mosh
 sudo apt-get install mosh -y
+
+sudo apt-get install git python-pip make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev
+sudo pip install virtualenvwrapper
+
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+echo 'pyenv virtualenvwrapper' >> ~/.zshrc
 
 echo "Installing and setting up pyenv"
 pyenv install 2.7.10
