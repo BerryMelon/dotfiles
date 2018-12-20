14168540ac58f3a4c91a75a09a43f891ec56973e
berrymelon <berrymelon@me.com>
2018-12-19 18:42:14 -0800

    added bootstrap

---
+++ Bootstrap/macos_install.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+echo "Berrymelon's bootstrap setups now starting"
+
+# Check for Homebrew, install if we don't have it
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Update homebrew recipes
+brew update
+
+# Install GNU core utilities (those that come with OS X are outdated)
+brew tap homebrew/dupes
+brew install coreutils
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-indent --with-default-names
+brew install gnu-which --with-default-names
+brew install gnu-grep --with-default-names
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+PACKAGES=(
+    autoconf
+    git
+    markdown
+    openssl
+    readline
+    sqlite3
+    xz
+    zlib
+    npm
+    python
+    python3
+    ssh-copy-id
+    vim
+    tmux
+    wget
+    zsh
+    zsh-completions
+    figlet
+    node
+    pyenv
+    pyenv-virtualenv
+    pkg-config
+    ruby
+    cmake
+    ctags
+    --with-clang llvm
+)
+
+echo "installing packages..."
+brew install ${PACKAGES[@]}
+
+echo "cleaning up..."
+brew cleanup
+
+# echo "Installing cask..."
+# brew install caskroom/cask/brew-cask
+# 
+# CASKS=(
+    # iterm2
+    # slack
+# )
+# 
+# echo "Installing cask apps..."
+# brew cask install ${CASKS[@]}
+
+echo "Installing and setting up pyenv"
+pyenv install 2.7.10
+pyenv install 3.7.0
+pyenv global 3.7.0
+
+source vimzsh.sh
+
+figlet Welcome Doheny.
---
+++ Bootstrap/vimzsh.sh
@@ -0,0 +1,54 @@
+echo "Installing oh-my-zsh"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+mkdir ~/tmp
+cd ~/tmp
+
+echo "Installing powerline fonts"
+# clone
+git clone https://github.com/powerline/fonts.git --depth=1
+# install
+cd fonts
+./install.sh
+# clean-up a bit
+cd ..
+rm -rf fonts
+
+[ -d "~/dotfiles" ] && mv ~/dotfiles ~/dotfiles_backup
+mkdir ~/dotfiles
+cd ~
+
+echo "Setting up dotfiles.."
+
+cd ../dotfiles
+
+echo "creating backups for current vimrc, zshrc, oh-my-zsh"
+mv ~/.vimrc ~/.vimrc_backup
+mv ~/.zshrc ~/.zshrc_backup
+mv ~/.oh-my-zsh ~/.oh-my-zsh_backup
+
+ln -s ~/dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/dotfiles/zsh/oh-my-zsh ~/.oh-my-zsh
+
+echo "creating backups for current tmux"
+mv ~/.tmux.conf ~/.tmux.conf_backup
+mv ~/.tmux.conf.local ~/.tmux.conf.local_backup
+mv ~/.tmux.conf.theme ~/.tmux.conf.theme_backup
+
+ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/tmux/tmux.conf.local ~/.tmux.conf.local
+ln -s ~/dotfiles/tmux/tmux.conf.theme ~/.tmux.conf.theme
+
+echo "Installing Vundle"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "Installing Vim plugins"
+vim +PluginInstall +qall
+
+echo "Setting up Plugin Configs.."
+cd ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
+
+zsh
+
+tmux new -s main
