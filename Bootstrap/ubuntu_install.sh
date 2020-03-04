1d12d0df01b4c3c94e2b9556338a69c37d3b18cf
berrymelon <berrymelon@me.com>
2020-03-04 02:13:21 +0000

    removed unused stuff

--- Bootstrap/ubuntu_install.sh
+++ Bootstrap/ubuntu_install.sh
@@ -2,13 +2,11 @@
 
 echo "Berrymelon's bootstrap setups now starting"
 
-
 sudo apt-get update
 sudo apt-get upgrade
 
 echo "installing packages..."
 sudo apt-get install git -y
-sudo apt-get install python -y
 sudo apt-get install python3 -y
 sudo apt-get install tmux -y
 sudo apt-get install zsh -y
@@ -22,19 +20,9 @@ sudo add-apt-repository ppa:keithw/mosh
 sudo apt-get install mosh -y
 
 sudo apt-get dist-upgrade -y
-sudo apt-get install build-essential python-dev python-setuptools python-pip python-smbus -y
-sudo apt-get install libncursesw5-dev libgdbm-dev libc6-dev -y
-sudo apt-get install zlib1g-dev libsqlite3-dev tk-dev -y
-sudo apt-get install libssl-dev openssl -y
-sudo apt-get install libffi-dev -y
-sudo apt-get install make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev -y
+sudo apt-get install openssl -y
 
 source vimzsh.sh
-
-# setup YCM if we have it
-if [ -d "$HOME/.vim/bundle/YouCompleteMe" ]; then
-    python3 $HOME/.vim/bundle/YouCompleteMe/install.py --clang-completer
-fi
 
 git config --global alias.tree "log --graph --decorate --pretty=oneline --abbrev-commit"
 
