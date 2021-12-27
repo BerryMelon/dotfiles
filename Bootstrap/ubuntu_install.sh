a7c0294ca22969ac8251ecdab65afdb7aa8f6ba2
EC2 Default User <ec2-user@ip-172-31-21-157.us-west-1.compute.internal>
2021-12-27 22:22:47 +0000

    added yum support for linux

--- Bootstrap/ubuntu_install.sh
+++ Bootstrap/ubuntu_install.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
+if command -v apt-get >/dev/null; then
+  echo "apt-get is used here"
+  pm="apt-get"
+elif command -v yum >/dev/null; then
+  echo "yum is used here"
+  pm="yum"
+else
+  echo "No recognizable pakage found. abort"
+  exit 0
+fi
+
 echo "Berrymelon's bootstrap setups now starting"
 
-sudo apt-get update
-sudo apt-get upgrade
+sudo $pm update
+sudo $pm upgrade
 
 echo "installing packages..."
-sudo apt-get install git -y
-sudo apt-get install python3 -y
-sudo apt-get install tmux -y
-sudo apt-get install zsh -y
-sudo apt-get install zsh-completions -y
-sudo apt-get install figlet -y
-sudo apt-get install cmake -y
-sudo apt-get install ctags -y
-sudo apt-get install python3-pip -y
-sudo apt-get install python3-venv -y
+sudo $pm install git -y
+sudo $pm install python3 -y
+sudo $pm install tmux -y
+sudo $pm install zsh -y
+sudo $pm install zsh-completions -y
+sudo $pm install figlet -y
+sudo $pm install cmake -y
+sudo $pm install ctags -y
+sudo $pm install python3-pip -y
+sudo $pm install python3-venv -y
 
-sudo apt-get install python-software-properties
-sudo add-apt-repository ppa:keithw/mosh
-sudo apt-get install mosh -y
+sudo $pm install python-software-properties
+sudo $pm install mosh -y
 
-sudo apt-get dist-upgrade -y
-sudo apt-get install openssl -y
-suto apt-get install openssh-server -y
+sudo $pm dist-upgrade -y
+sudo $pm install openssl -y
+suto $pm install openssh-server -y
 update-rc.d -f ssh remove
 update-rc.d -f ssh defaults
 
