fe385de5ce0b000b14d16fe1ee8f430ce85bd8dc
Ubuntu <ubuntu@ip-172-31-26-21.us-west-1.compute.internal>
2025-06-20 05:35:12 +0000

    adding docker support

--- Bootstrap/ubuntu_install.sh
+++ Bootstrap/ubuntu_install.sh
@@ -41,6 +41,19 @@ sudo $pm install openssl -y
 sudo $pm install openssh-server -y
 sudo $pm install neovim -y
 
+# Setting up Docker
+sudo $pm update
+sudo $pm install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo $pm update
+sudo $pm install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 update-rc.d -f ssh remove
 update-rc.d -f ssh defaults
 
